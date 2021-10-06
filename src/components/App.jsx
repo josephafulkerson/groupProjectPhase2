a531706a038a350b2c25aebfeb31012a3b6fb56b
Hashem Gray <Hgray1109@gmail.com>
2021-10-06 07:58:36 -0400

    Swan

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 import NavBar from "./NavBar";
 import Header from "./Header";
+import useState from 'react'
 
+const Url = 'http://localhost:8000/equipment'
+
+const [members, setMembers] = useState([])
 
 function App() {
   return (
     <div>
       <Header />
       <NavBar />
+      <JoinNow addMember={addMember} />
     </div>
   );
 }
+
+function addMember(newMember) {
+  fetch(Url, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body:JSON.stringify(newMember)})
+    .then(resp => resp.json())
+    .then(resp => {setMembers([...members, resp])})
+  }
+  
 
 export default App;
 
--- src/components/JoinNow.jsx
+++ src/components/JoinNow.jsx
@@ -1,9 +1,80 @@
 import React from 'react'
+import useState from 'react'
 
-function JoinNow() {
+function JoinNow (addMember) {
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [phoneNumber, setPhoneNumber] = useState('')
+    const [medals, setMedals] = useState('')
+    const [bloodType, setBloodType] = useState('')
+    const [email, setEmail] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault() 
+        const app = { firstName, lastName, phoneNumber, medals, bloodType, email}
+        console.log(app)
+    }
+
     return (
-        <div>
-            
+        <div className="newMemberForm">
+            <h2>Submit Application</h2>
+            <form>
+            <label> First Name: </label>
+                <textarea 
+                    type="text"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+            <label> Last Name:</label>
+                <textarea 
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                />
+            <label> Membership: </label>
+                <select
+                    value={medals}
+                    onChange={(e) => setMedals(e.target.value)}
+                    >
+                    <option value="Bronze">Bronze</option>
+                    <option value="Silver">Silver</option>
+                    <optoin value="Gold">Gold</optoin>
+                </select>
+            <label> Phone Number: </label>
+                <textarea 
+                name="description" 
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+                />
+
+            <label> Email Address: </label>
+                <input 
+                    type="email" 
+                    name="description"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
+            <label> Blood Type: </label>
+                <select
+                    value={bloodType}
+                    onChange={(e) => setBloodType(e.target.value)}
+                    >
+                    <option value="Type A+">Type A+</option>
+                    <option value="Type A-">Type A-</option>
+                    <option value="Type B+">Type B+</option>
+                    <option value="Type B-">Type B-</option>
+                    <option value="Type AB+">Type AB+</option>
+                    <option value="Type AB-">Type AB-</option>
+                    <option value="Type O+">Type O+</option>
+                    <option value="Type O-">Type O-</option>
+                </select>
+                
+                {/* OnChange => setFormData */}
+                {/* Aler on submit */}
+
+            <input onSubmit={(e) => {addMember(firstName, lastName, phoneNumber, medals, bloodType, email);e.preventDefault()}} type="submit" value="Apply Now"/>
+        </form>
         </div>
     )
 }
