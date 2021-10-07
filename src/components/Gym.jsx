bd312791b9b9028049b9f684f42231b64fd7f0d5
Hashem Gray <Hgray1109@gmail.com>
2021-10-07 05:22:16 -0400

    Swan Writing

--- src/components/Gym.jsx
+++ src/components/Gym.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import GymCard from './GymCard'
 import { BASE_API_URL } from "../const";
+import Home from "./Home"
 
 
 function Gym() {
     const [ equipment, setEquipment ] = useState([])
+    const [ facility, setFacility ] = useState([])
+
 
     useEffect(() => {
         fetch(`${BASE_API_URL}/equipment`)
@@ -12,12 +15,19 @@ function Gym() {
         .then(data => setEquipment(data))
     }, [])
 
+    useEffect(() => {
+        fetch(`${BASE_API_URL}/facility`)
+        .then(r => r.json())
+        .then(data => setFacility(data)) 
+            // setFacility(data))
+    }, [])
 
 
     return (
         <div>
             <p>TEST</p>
             {equipment.map(items => <GymCard equipment={items} key={items.id} />)}
+            {facility.map(stock => <Home facility={stock} key={stock.id} />)}
         </div>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import styled from "styled-components";
 
-const Home = () => {
+const Home = ({facility}) => {
+    const {id, name, image} = facility
+
+    const images = require.context('../gymBuilding', true)
+    let facilityImage = images(`./${image}`).default;
+
     return (
-        <div>
+        <div style={{display: 'inline-block'}}>
+        <Wrapper>
+        <img src={facilityImage} alt={name} key={id}
+        width='200px' height='200px' style={{borderRadius:"5%"}}/>
+        </Wrapper>
             <p></p>
         </div>
     )
 }
 
-export default Home
+export default Home;
+
+const Wrapper = styled.div`
+    border-radius: 50%;
+`;
